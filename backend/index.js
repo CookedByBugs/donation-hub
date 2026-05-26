b8aa60d2831c56fa837961e916a95362ca45354b
mernbit <muhammadhashir5889@gmail.com>
2026-05-26 13:24:22 +0500

    vercel config added

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // explicit preflight handling
+app.options("/*", (req, res) => res.sendStatus(200));
 app.use(express.json());
 
 connectDB();
